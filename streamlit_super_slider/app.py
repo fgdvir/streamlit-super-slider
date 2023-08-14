5cab191004ba27eb774ba8d09d043620490ee381
Dvir Itzkovits <dviri@mobileye.com>
2023-08-14 07:25:20 +0300

    example app

--- streamlit_super_slider/app.py
+++ streamlit_super_slider/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_super_slider import st_slider
 
-
-value = st_slider(0, 100, 50, key="my_slider")
+max_val = 1234567890
+value = st_slider(0, max_val, max_val-1, key="my_slider")
 st.subheader(f"value: {value}")
